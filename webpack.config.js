80820f7b48df45955da3c93138a28f478b213050
나비 <hbh2692@naver.com>
2017-10-17 05:25:10 +0900

    css 룰 추가

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,14 @@ module.exports = {
                 }
             },
             {
+                test:/\.css$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }]
+            },
+            {
                 test:/\.scss$/,
                 use: [{
                     loader: "style-loader"
