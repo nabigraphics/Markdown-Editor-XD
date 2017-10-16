e0514f4c8cd55bd02a095b282f3700bd35f6db82
나비 <hbh2692@naver.com>
2017-10-16 09:01:46 +0900

    init_setting

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+module.exports = {
+    entry:{
+        bundle:['./src/app.js','./src/scss/style.scss']
+    },
+    output:{
+        path:'/dist',
+        filename:'[name].js',
+        publicPath:'/'
+    },
+    devServer: {
+        hot:true,
+        inline: true,
+        port: 25252,
+        host:'0.0.0.0',
+        contentBase: __dirname + '/dist/',
+        historyApiFallback: true,
+    },
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                        presets: ['env','react']
+                    }
+                }
+            },
+            {
+                test:/\.scss$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader"
+                }]
+            }
+        ]
+    },
+    plugins:[
+        new webpack.HotModuleReplacementPlugin()
+    ],  
+    resolve:{
+        modules:['node_modules'],
+        extensions:['.js','.scss']
+    }
+}
